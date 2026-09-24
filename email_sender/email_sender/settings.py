78b00be38c550dff567c342096dd40d87293335f
Mohammed Risal <94342524+Devriz1@users.noreply.github.com>
2026-09-24 17:13:25 +0530

    Restore original settings.py content

--- email_sender/email_sender/settings.py
+++ email_sender/email_sender/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rhambogamer12@gmail.com'  # Change this
-EMAIL_HOST_PASSWORD = 'slbj kjvc tvwg yrbv'  # Change this
+EMAIL_HOST_PASSWORD = '111'  # Change this
 
 
 # Application definition
